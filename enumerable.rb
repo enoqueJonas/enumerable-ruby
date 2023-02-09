e5bdeeef7d76b584c62ad076ea022a38c59da3cd
Simon Gideon <simongideon918@gmail.com>
2023-02-10 01:35:29 +0300

    Update enumerable.rb

--- enumerable.rb
+++ enumerable.rb
@@ -13,6 +13,7 @@ class MyList
 end
 list = MyList.new(1, 2, 3, 4)
 list.all? {|e| e < 5}
-list.all? {|e| e > 5}                                                                                                                                          list.any? {|e| e == 2}
+list.all? {|e| e > 5}
+list.any? {|e| e == 2}
 list.any? {|e| e == 5}
 list.filter {|e| e.even?}
