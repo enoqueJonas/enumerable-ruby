57f4154e9b20bb789aabbf0821852c5cb88581c9
enoqueJonas <enoque.macanda@gmail.com>
2023-02-09 18:46:31 +0200

    UPDATE module.rb

--- module.rb
+++ module.rb
@@ -1,3 +1,17 @@
 module MyEnumerable 
+    def all?
+        each {|n| return false unless yield n}
+        true
+    end
 
+    def any?
+        each {|n| return true if yield n}
+        false
+    end
+
+    def filter 
+        filtered = []
+        each {|n| filtered.push(e) if yield e}
+        filtered
+    end
 end
