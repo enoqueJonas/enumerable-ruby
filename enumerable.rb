7250ca8a6d438fa49132d8d269b539e8e1180ff9
enoqueJonas <enoque.macanda@gmail.com>
2023-02-09 19:19:53 +0200

    FIX linters

--- enumerable.rb
+++ enumerable.rb
@@ -1,14 +1,12 @@
 require_relative './enumerable'
 
 class MyList
-    include MyEnumerable;
-    def initialize(*list)
-        @list = list
-    end
+  include MyEnumerable;
+  def initialize(*list)
+    @list = list
+  end
 
-    def each(&block)
-        @list.each(&block)
-    end
+  def each(&block)
+    @list.each(&block)
+  end
 end
-
-
--- module.rb
+++ module.rb
@@ -1,17 +1,17 @@
-module MyEnumerable 
-    def all?
-        each {|n| return false unless yield n}
-        true
-    end
+module MyEnumerable
+  def all?
+    each { |n| return false unless yield n }
+    true
+  end
 
-    def any?
-        each {|n| return true if yield n}
-        false
-    end
+  def any?
+    each { |n| return true if yield n }
+    false
+  end
 
-    def filter 
-        filtered = []
-        each {|n| filtered.push(n) if yield n}
-        filtered
-    end
+  def filter
+    filtered = []
+    each { |n| filtered.push(n) if yield n }
+    filtered
+  end
 end
